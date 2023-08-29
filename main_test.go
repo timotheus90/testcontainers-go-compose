162364f372b0f8685ec972fd9ba0801d38af22c7
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-29 12:49:34 +0200

    Initial commit, using testcontainers-go v0.23.0

---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	tc "github.com/testcontainers/testcontainers-go/modules/compose"
+)
+
+func TestCompose(t *testing.T) {
+	compose, err := tc.NewDockerCompose(filepath.Join("testdata", "docker-compose.yml"))
+	assert.NoError(t, err, "NewDockerComposeAPI()")
+
+	t.Cleanup(func() {
+		assert.NoError(t, compose.Down(context.Background(), tc.RemoveOrphans(true), tc.RemoveImagesLocal), "compose.Down()")
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	assert.NoError(t, compose.Up(ctx, tc.Wait(true)), "compose.Up()")
+
+	// do some testing here
+}
